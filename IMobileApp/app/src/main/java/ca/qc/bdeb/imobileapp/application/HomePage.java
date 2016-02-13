6a8e690838b64a64c7e5cbeba0b75cd7198993e6
Al3xandr3GitHub <alexandre-sasseville@hotmail.com>
2016-02-13 00:41:02 -0500

    Add bluetooth stuff

--- IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/HomePage.java
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/HomePage.java
@@ -1,5 +1,6 @@
 package ca.qc.bdeb.imobileapp.application;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,14 +9,49 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import ca.qc.bdeb.imobileapp.R;
+import ca.qc.bdeb.imobileapp.modele.bluetoothscanner.ListActivity;
 import ca.qc.bdeb.imobileapp.modele.persistence.DbHelper;
+import mehdi.sakout.fancybuttons.FancyButton;
 
 
 public class HomePage extends AppCompatActivity {
+
+    FancyButton btnSend;
+    FancyButton btnReceive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        initializeComponent();
+        initializeBtnsClick();
+    }
+
+    private void initializeComponent() {
+
+        btnSend = (FancyButton) findViewById(R.id.home_page_btn_envoyer);
+        btnReceive = (FancyButton) findViewById(R.id.home_page_btn_recevoir);
+
+    }
+
+    private void initializeBtnsClick(){
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnReceive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
