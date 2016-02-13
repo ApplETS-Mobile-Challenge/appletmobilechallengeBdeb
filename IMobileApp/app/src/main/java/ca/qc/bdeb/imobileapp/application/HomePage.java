5916961da1ae9f1ac0d65e4f93c6e0ed3bb37440
Vincent Bissonnette <bissonnette@gmail.com>
2016-02-13 04:03:46 -0500

    Bd finish

--- IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/HomePage.java
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/HomePage.java
@@ -8,8 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import ca.qc.bdeb.imobileapp.R;
 import ca.qc.bdeb.imobileapp.modele.bluetoothscanner.ListActivity;
+import ca.qc.bdeb.imobileapp.modele.objectModel.Question;
+import ca.qc.bdeb.imobileapp.modele.objectModel.Questionnaire;
+import ca.qc.bdeb.imobileapp.modele.objectModel.QuestionnaireTemplate;
 import ca.qc.bdeb.imobileapp.modele.persistence.DbHelper;
 import mehdi.sakout.fancybuttons.FancyButton;
 
@@ -23,6 +29,19 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Question question = new Question(0, "tegg", 0);
+        question.addAnswerChoices("aaa", true);
+        question.addAnswerChoices("aggs", false);
+        ArrayList<Question> ll = new ArrayList<>();
+        ll.add(question);
+        ll.add(question);
+        Questionnaire ques = new Questionnaire(0, "test", new Date(123123), new Date(123123));
+        ques.setQuestionList(ll);
+        DbHelper accesBd = DbHelper.getInstance(getApplicationContext());
+        int id = accesBd.insertNewQuestionnaire(ques);
+        ArrayList<QuestionnaireTemplate> ddd = accesBd.getAllQuestionnaire();
+        Questionnaire test = accesBd.getOneQuestionnaire(1);
+        accesBd.deleteQuestionnaire(ques);
 
         initializeComponent();
         initializeBtnsClick();
--- IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/modele/persistence/DbHelper.java
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/modele/persistence/DbHelper.java
@@ -2,17 +2,22 @@ package ca.qc.bdeb.imobileapp.modele.persistence;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import ca.qc.bdeb.imobileapp.modele.objectModel.Question;
 import ca.qc.bdeb.imobileapp.modele.objectModel.Questionnaire;
+import ca.qc.bdeb.imobileapp.modele.objectModel.QuestionnaireTemplate;
+
+import static ca.qc.bdeb.imobileapp.modele.objectModel.Questionnaire.*;
 
 /**
  * Created by Olivier on 2015-09-25.
@@ -104,7 +109,7 @@ public class DbHelper extends SQLiteOpenHelper {
         db.execSQL(sqlLine);
     }
 
-    public void insertNewQuestionnaire(Questionnaire questionnaire) {
+    public int insertNewQuestionnaire(Questionnaire questionnaire) {
         SQLiteDatabase db;
         ContentValues values = new ContentValues();
 
@@ -115,6 +120,7 @@ public class DbHelper extends SQLiteOpenHelper {
         int questionnaireId = (int) db.insert(QUESTIONNAIRE_TABLE_NAME, null, values);
         db.close();
         insertNewQuestions(questionnaire.getQuestionList(), questionnaireId);
+        return questionnaireId;
     }
 
     public void insertNewQuestions(ArrayList<Question> questions, int questionnaireId) {
@@ -188,21 +194,110 @@ public class DbHelper extends SQLiteOpenHelper {
         db.close();
     }
 
-    public void updateAnswerChoices(int answerChoiceId, String answerChoice, boolean veracity) {
+    public void updateAnswerChoices(int questionId, String answerChoice, boolean veracity) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(ANSWER_CHOICE_CHOICE, answerChoice);
         values.put(ANSWER_VERACITY, veracity ? 1 : 0);
-        db.update(ANSWER_CHOICES_TABLE_NAME, values, ANSWER_CHOICE_CHOICE + "=?",
-                new String[]{Integer.toString(answerChoiceId)});
+        db.update(ANSWER_CHOICES_TABLE_NAME, values, ANSWER_QUESTION_REFERENCE_ID + "=?",
+                new String[]{Integer.toString(questionId)});
         db.close();
     }
 
-    public void updateQuestion(int questionId) {
-
+    public void updateQuestion(Question question) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(QUESTION_QUESTION, question.getQuestion());
+        db.update(QUESTION_TABLE_NAME, values, QUESTION_ID + "=?",
+                new String[]{Integer.toString(question.getQuestionId())});
+        db.close();
     }
 
-    public void getAllQuestionnaire() {
+    public ArrayList<QuestionnaireTemplate> getAllQuestionnaire() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<QuestionnaireTemplate> templates = new ArrayList<>();
+        QuestionnaireTemplate template = new QuestionnaireTemplate();
+        String query = "select " + QUESTIONNAIRE_ID + ", " + QUESTIONNAIRE_NAME + ", "
+                + QUESTIONNAIRE_EDIT_DATE + " from " + QUESTIONNAIRE_TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
 
+        if(cursor != null && cursor.getCount() != 0) {
+            cursor.moveToFirst();
+            template.questionnaireId = Integer.parseInt(cursor.getString(0));
+            template.questionnaireName = cursor.getString(1);
+            template.editDate = new Date(Long.parseLong(cursor.getString(2)));
+            templates.add(template);
+            while(cursor.moveToNext()) {
+                template = new QuestionnaireTemplate();
+                template.questionnaireId = Integer.parseInt(cursor.getString(0));
+                template.questionnaireName = cursor.getString(1);
+                template.editDate = new Date(Long.parseLong(cursor.getString(2)));
+                templates.add(template);
+            }
+        }
+        return templates;
+    }
+
+    public Questionnaire getOneQuestionnaire(int questionnaireId) {
+        Questionnaire questionnaire = null;
+        String query = "select * from " + QUESTIONNAIRE_TABLE_NAME + " where "
+                + QUESTIONNAIRE_ID + "=" + questionnaireId;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor != null && cursor.getCount() != 0) {
+            cursor.moveToFirst();
+            questionnaire = new Questionnaire(Integer.parseInt(cursor.getString(0)),
+                    cursor.getString(1), new Date(Long.parseLong(cursor.getString(2))),
+                    new Date(Long.parseLong(cursor.getString(3))));
+            db.close();
+            questionnaire.setQuestionList(getQuestionnaireQuestions(questionnaireId));
+        }
+        return questionnaire;
+    }
+
+    private ArrayList<Question> getQuestionnaireQuestions(int questionnaireId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Question question = null;
+        ArrayList<Question> questions = new ArrayList<>();
+        String query = "select * from " + QUESTION_TABLE_NAME + " where " + QUESTION_QUESTIONNAIRE_REFERENCE_ID
+                + "=" + questionnaireId;
+
+       // String query = "select * from " + QUESTION_TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor != null && cursor.getCount() != 0) {
+            cursor.moveToFirst();
+             question = new Question(Integer.parseInt(cursor.getString(0)),
+                    cursor.getString(1), Integer.parseInt(cursor.getString(2)));
+            questions.add(question);
+            while (cursor.moveToNext()) {
+                question = new Question(Integer.parseInt(cursor.getString(0)),
+                        cursor.getString(1), Integer.parseInt(cursor.getString(2)));
+                questions.add(question);
+            }
+            db.close();
+        }
+        getQuestionAnswerChoices(questions);
+        return questions;
+    }
+
+    private void getQuestionAnswerChoices(ArrayList<Question> questions) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query;
+        Cursor cursor = null;
+        for(int i = 0; i < questions.size(); ++i) {
+            query = "select " + ANSWER_CHOICE_CHOICE + ", " + ANSWER_VERACITY + " from "
+                    + ANSWER_CHOICES_TABLE_NAME + " where " + ANSWER_QUESTION_REFERENCE_ID
+                    + "=" + questions.get(i).getQuestionId();
+            cursor = db.rawQuery(query, null);
+            if(cursor.moveToFirst()) {
+                questions.get(i).addAnswerChoices(cursor.getString(0),
+                        Integer.parseInt(cursor.getString(1)) == 1);
+                while (cursor.moveToNext()) {
+                    questions.get(i).addAnswerChoices(cursor.getString(0),
+                            Integer.parseInt(cursor.getString(1)) == 1);
+                }
+            }
+        }
     }
 }
