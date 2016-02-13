9a66d74dc3fffbcd0477eae8e6ad77a4db662647
castropeo <casof_mexc@hotmail.com>
2016-02-12 22:03:05 -0500

    Supression du main replace par home page

---
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/HomePage.java
@@ -0,0 +1,22 @@
+package ca.qc.bdeb.imobileapp.application;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ca.qc.bdeb.imobileapp.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+
+
+    }
+}
