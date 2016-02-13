bdda08f06f837d279c1c1aa58762a153d66e6fd5
castropeo <casof_mexc@hotmail.com>
2016-02-13 11:21:24 -0500

    ajout a patir de la list de survey

--- IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/List_Survey.java
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/List_Survey.java
@@ -45,8 +45,8 @@ public class List_Survey extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(List_Survey.this, Create_Survey.class);
+                startActivity(intent);
 
             }
         });
