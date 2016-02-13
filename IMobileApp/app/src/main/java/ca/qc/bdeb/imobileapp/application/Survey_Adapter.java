747d6acc2fc564965e8163b2ea247c5e36fc4057
Hippolyte <hippolyte.glaus@gmail.com>
2016-02-13 02:04:59 -0500

    Create list survey

---
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/Survey_Adapter.java
@@ -0,0 +1,55 @@
+package ca.qc.bdeb.imobileapp.application;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.List;
+import ca.qc.bdeb.imobileapp.R;
+import ca.qc.bdeb.imobileapp.modele.objectModel.Questionnaire;
+
+/**
+ * Created by Hippolyte Glaus on 13/02/16.
+ */
+public class Survey_Adapter extends ArrayAdapter<Questionnaire> {
+    private Context context;
+    List<Questionnaire> items;
+
+    public Survey_Adapter(Context context, int resource, List<Questionnaire> items) {
+        super(context, resource);
+        this.context = context;
+        this.items = items;
+    }
+
+    private class Survey {
+        TextView title;
+        TextView nbr_question;
+        TextView creation_Date;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Survey holder = null;
+        Questionnaire rowItem = getItem(position);
+
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            holder = new Survey();
+            convertView = mInflater.inflate(R.layout.layout_list_survey, null);
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(R.id.survey_List_nom);
+            holder.nbr_question = (TextView) convertView.findViewById(R.id.survey_list_nbrQuestion);
+
+        } else {
+            holder = (Survey) convertView.getTag();
+        }
+
+        return convertView;
+    }
+
+}
