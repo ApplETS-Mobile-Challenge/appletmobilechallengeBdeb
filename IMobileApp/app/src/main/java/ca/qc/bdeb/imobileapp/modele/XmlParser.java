a684c6c986916ede49f9c2cba18397db962996af
Hippolyte <hippolyte.glaus@gmail.com>
2016-02-12 23:28:36 -0500

    parse questionnaire to xml

---
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/modele/XmlParser.java
@@ -0,0 +1,33 @@
+package ca.qc.bdeb.imobileapp.modele;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import ca.qc.bdeb.imobileapp.modele.objectModel.Questionnaire;
+import ca.qc.bdeb.imobileapp.modele.persistence.DbHelper;
+
+/**
+ * Created by Hippolyte Glaus on 12/02/16.
+ */
+public class XmlParser
+{
+
+    private DbHelper db;
+    public XmlParser(DbHelper db){
+        this.db = db;
+    }
+
+    public void ParseToXml(Questionnaire questionnaire){
+        XStream xstream = new XStream(new DomDriver());
+        String xml = xstream.toXML(questionnaire);
+        ReadFromXml(xml);
+    }
+
+    public void ReadFromXml(String data){
+        XStream xstream = new XStream(new DomDriver());
+        Questionnaire newQuestionnaire = (Questionnaire)xstream.fromXML(data);
+        //todo Add to bd
+//        db.saveQuestionnaire;
+    }
+
+
+}
