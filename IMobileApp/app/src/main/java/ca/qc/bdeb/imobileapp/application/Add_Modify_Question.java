309e91fe8d53f39b7d17ebb59cfb212ab45f8d46
castropeo <casof_mexc@hotmail.com>
2016-02-13 08:13:16 -0500

    Ajout delete menu

--- IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/Add_Modify_Question.java
+++ IMobileApp/app/src/main/java/ca/qc/bdeb/imobileapp/application/Add_Modify_Question.java
@@ -5,6 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import ca.qc.bdeb.imobileapp.R;
@@ -27,6 +30,28 @@ public class Add_Modify_Question extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_delete, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if(id == R.id.action_delete){
+            //
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
